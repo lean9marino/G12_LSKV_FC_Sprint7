aa0b465c22c56840628467311db48c0f2c109fa8
lean9ema <lean9marino@gmail.com>
2022-04-02 12:21:31 -0300

    Lean-arreglos

--- src/middlewares/product-edit-validator.js
+++ src/middlewares/product-edit-validator.js
@@ -1,10 +1,10 @@
 const {body} = require('express-validator'); 
 
 const validator =[ 
-    body('name').notEmpty().withMessage('Debe escribir un nombre.'),
-    body('price').notEmpty().withMessage('Debe escribir un precio.'), 
-    body('color').notEmpty().withMessage('Debe elegir almenos un color.'), 
-    body('sizes').notEmpty().withMessage('Debe elegir almenos un talle.'), 
+    body('name').notEmpty().withMessage('Debe escribir un nombre.').bail(),
+    body('price').notEmpty().withMessage('Debe escribir un precio.').bail(), 
+    body('color').notEmpty().withMessage('Debe elegir almenos un color.').bail(), 
+    body('sizes').notEmpty().withMessage('Debe elegir almenos un talle.').bail(), 
     body('description').notEmpty().withMessage('Debe escribir una descripcion.').bail()
         .isLength({min:30}).withMessage('Debe escribir como minimo 30 letras u caracteres.')
 ]
